31e33820a93f81c8d32248843d5a6f5ea1d7a4b7
MjFriday <7amlhaalan@gmail.com>
2026-09-11 18:39:24 +0300

    i enslaved AI to make the code better now perfect catches are very accurate

--- macroScript.cpp
+++ macroScript.cpp
@@ -1,18 +1,38 @@
 #include <iostream>
 #include <fstream>
 #include <cmath>
-#include <algorithm>
 #include <chrono>
+#include <cstdint>
+#include <unistd.h>
 #include <X11/Xlib.h>
 #include <X11/extensions/XTest.h>
-#include <unistd.h>
-#include <cstdint>
 #include <X11/keysym.h>
 #include <X11/Xutil.h>
 
 using namespace std;
 
-const string default_settings="screen_resolution: 1366,768\n\n# pixel location settings\n# upper left corner of the fishing mini game bar (X,Y)\nbar_start:407,642\n# bottom right corner of the fishing mini game bar (X,Y)\nbar_end:958,664\n# friend boost icon location. only 1 green pixel from it is needed\nfriend_boost:30,718\n# you must have the rod in your hot bar and have a pixel location of the white outline around it\nrod_equip:373,700\n\n# pixel color settings (very self explanatory)\nctrl_bar_clr_L:0xF1F1F1\nctrl_bar_clr_R:0xF1F1F1\narrow_clr:0x868686\nfish_minigame_clr:0x434B5B\nfriend_boost_clr:0x9BFF9B\n\ncasting:1\nshaking:1\nfishing:1";
+bool should_exit = false;
+
+const string default_settings = 
+    "screen_resolution: 1366,768\n\n"
+    "# pixel location settings\n"
+    "# upper left corner of the fishing mini game bar (X,Y)\n"
+    "bar_start:407,642\n"
+    "# bottom right corner of the fishing mini game bar (X,Y)\n"
+    "bar_end:958,664\n"
+    "# friend boost icon location. only 1 green pixel from it is needed\n"
+    "friend_boost:30,718\n"
+    "# you must have the rod in your hot bar and have a pixel location of the white outline around it\n"
+    "rod_equip:373,700\n\n"
+    "# pixel color settings (very self explanatory)\n"
+    "ctrl_bar_clr_L:0xF1F1F1\n"
+    "ctrl_bar_clr_R:0xF1F1F1\n"
+    "arrow_clr:0x868686\n"
+    "fish_minigame_clr:0x434B5B\n"
+    "friend_boost_clr:0x9BFF9B\n\n"
+    "casting:1\n"
+    "shaking:0\n"
+    "fishing:1\n";
 
 struct settings;
 void load_settings(settings& s);
@@ -54,7 +74,7 @@ void load_settings(settings& s)
     s.display = XOpenDisplay(NULL);
     if (!s.display) 
     {
-        std::cerr << "\033[31mAre you sure you are running under an X11 display?\033[0m" << std::endl;
+        cerr << "\033[31mAre you sure you are running under an X11 display?\033[0m" << endl;
         exit(1);
     }
     s.root = DefaultRootWindow(s.display);
@@ -66,24 +86,23 @@ void load_settings(settings& s)
         while(getline(fetchfile, line))
         {
             if(line.empty() || line[0] == '#') continue;
-            else
-            {
-                string key = line.substr(0, line.find(':'));
-                string value = line.substr(line.find(':') + 1);
-                if(key == "screen_resolution") str_to_x_and_y(value, s.screen_res);
-                else if(key == "bar_start") str_to_x_and_y(value, s.bar_start);
-                else if(key == "bar_end") str_to_x_and_y(value, s.bar_end);
-                else if(key == "friend_boost") str_to_x_and_y(value, s.friend_boost);
-                else if(key == "rod_equip") str_to_x_and_y(value, s.rod_equip);
-                else if(key == "ctrl_bar_clr_L") s.ctrl_bar_clr_L = stoul(value, nullptr, 16);
-                else if(key == "ctrl_bar_clr_R") s.ctrl_bar_clr_R = stoul(value, nullptr, 16);
-                else if(key == "arrow_clr") s.arrow_clr = stoul(value, nullptr, 16);
-                else if(key == "fish_minigame_clr") s.fish_minigame_clr = stoul(value, nullptr, 16);
-                else if(key == "friend_boost_clr") s.friend_boost_clr = stoul(value, nullptr, 16);
-                else if(key == "casting") s.casting = stoi(value);
-                else if(key == "shaking") s.shaking = stoi(value);
-                else if(key == "fishing") s.fishing = stoi(value);
-            }
+            
+            string key = line.substr(0, line.find(':'));
+            string value = line.substr(line.find(':') + 1);
+            
+            if(key == "screen_resolution") str_to_x_and_y(value, s.screen_res);
+            else if(key == "bar_start") str_to_x_and_y(value, s.bar_start);
+            else if(key == "bar_end") str_to_x_and_y(value, s.bar_end);
+            else if(key == "friend_boost") str_to_x_and_y(value, s.friend_boost);
+            else if(key == "rod_equip") str_to_x_and_y(value, s.rod_equip);
+            else if(key == "ctrl_bar_clr_L") s.ctrl_bar_clr_L = stoul(value, nullptr, 16);
+            else if(key == "ctrl_bar_clr_R") s.ctrl_bar_clr_R = stoul(value, nullptr, 16);
+            else if(key == "arrow_clr") s.arrow_clr = stoul(value, nullptr, 16);
+            else if(key == "fish_minigame_clr") s.fish_minigame_clr = stoul(value, nullptr, 16);
+            else if(key == "friend_boost_clr") s.friend_boost_clr = stoul(value, nullptr, 16);
+            else if(key == "casting") s.casting = stoi(value);
+            else if(key == "shaking") s.shaking = stoi(value);
+            else if(key == "fishing") s.fishing = stoi(value);
         }
         fetchfile.close();
     }
@@ -93,6 +112,7 @@ void load_settings(settings& s)
         ofstream createfile("settings.dat");
         createfile << default_settings;
         createfile.close();
+        
         s.screen_res[0]=1366; s.screen_res[1]=768;
         s.bar_start[0]=407; s.bar_start[1]=642;
         s.bar_end[0]=958; s.bar_end[1]=664;
@@ -113,46 +133,38 @@ bool frnd_icon_check(settings& s)
     return active;
 }
 
-int rod_equiping(settings& s, bool equip_it=true)
+void rod_equiping(settings& s)
 {
     XImage* check = XGetImage(s.display, s.root, s.rod_equip[0], s.rod_equip[1], 1, 1, AllPlanes, ZPixmap);
-    if(0xFFFFFF != ((uint32_t)XGetPixel(check, 0, 0) & 0xFFFFFF) && equip_it)
+    if(0xFFFFFF != ((uint32_t)XGetPixel(check, 0, 0) & 0xFFFFFF))
     {
         XTestFakeKeyEvent(s.display, XKeysymToKeycode(s.display, XK_1), 1, CurrentTime);
         XFlush(s.display); usleep(10000);
         XTestFakeKeyEvent(s.display, XKeysymToKeycode(s.display, XK_1), 0, CurrentTime);
         XFlush(s.display); usleep(10000);
-    }
-    else
-    {
-        XTestFakeKeyEvent(s.display, XKeysymToKeycode(s.display, XK_1), 1, CurrentTime);
-        XFlush(s.display);
-        usleep(1000);
-        XTestFakeKeyEvent(s.display, XKeysymToKeycode(s.display, XK_1), 0, CurrentTime);
-        XFlush(s.display);
     }
     XDestroyImage(check);
-    return 1;
 }
 
-int cast_(settings& s)
+void cast_rod(settings& s)
 {
     if(frnd_icon_check(s))
     {
         rod_equiping(s);
-        XTestFakeMotionEvent(s.display, -1, (s.screen_res[0] / 2), (s.screen_res[1] / 2), CurrentTime);
-        XFlush(s.display); usleep(100000);
+        XTestFakeMotionEvent(s.display, -1, (s.screen_res[0] / 2), ((s.screen_res[1] / 2)+5), CurrentTime);
+        XFlush(s.display); usleep(1000);
         XTestFakeButtonEvent(s.display, 1, 1, CurrentTime);
         XFlush(s.display); usleep(200000);
+        XTestFakeMotionEvent(s.display, -1, (s.screen_res[0] / 2), ((s.screen_res[1] / 2)), CurrentTime);
+        XFlush(s.display); usleep(1000);
         XTestFakeButtonEvent(s.display, 1, 0, CurrentTime);
-        XFlush(s.display); usleep(10000);
+        XFlush(s.display);
     }
-    return 0;
 }
 
-int shake_(settings& s)
+void shake_rod(settings& s)
 {
-    while(1)
+    while(!should_exit)
     {
         if(frnd_icon_check(s))
         {
@@ -163,50 +175,55 @@ int shake_(settings& s)
         }
         else
         {
-            return 0;
+            return;
         }
     }
 }
 
-int fishing_(settings& s){
+void play_minigame(settings& s)
+{
     int mini_game_bar_[3] = {s.bar_start[0], s.bar_end[0], (s.bar_start[1] + s.bar_end[1]) / 2};
     int bar_width = mini_game_bar_[1] - mini_game_bar_[0];
 
-    constexpr float Kp = 0.18f;
-    constexpr float Kd_base = 1.40f;
+    constexpr float Kp = 0.10f;
+    constexpr float Kd_base = 1.0f;
     constexpr float max_velocity = 0.25f;
     constexpr float base_brake_lead_hold = 155.0f;
     constexpr float brake_lead_release = 75.0f;
     constexpr float quad_brake_factor = 180.0f;
-    constexpr float alpha = 0.32f;
+    constexpr float alpha = 0.8f;
     constexpr float max_kd_force = 12.0f;
     constexpr int coast_zone = 12;
-    constexpr int deadband = 5;
+    constexpr int deadband = 1;
+    constexpr float target_alpha = 0.5f;
 
     float last_bar_pos = -1.0f;
     float smoothed_velocity = 0.0f;
+    float smoothed_target = bar_width / 2.0f;
     bool is_holding = false;
 
     auto last_time = chrono::high_resolution_clock::now();
-
-    while(1)
+    int frnd_check_count = 0;
+    
+    while(!should_exit)
     {
         if(frnd_icon_check(s)) 
         {
-            usleep(100000);
-            rod_equiping(s, false);
-            return 0;
+            frnd_check_count++;
+            if(frnd_check_count > 50) return;
         }
+        else frnd_check_count = 0;
+        
         XImage* check_bar = XGetImage(s.display, s.root, mini_game_bar_[0], mini_game_bar_[2], bar_width, 1, AllPlanes, ZPixmap);
         int ctrl_bar_pos[2] = {0, bar_width - 1};
-        int priority_zone = bar_width / 2;
 
         for(int i = 0; i < bar_width - 1; i++)
         {
             uint32_t clr = (uint32_t)XGetPixel(check_bar, i, 0) & 0xFFFFFF;
             if(s.ctrl_bar_clr_L == clr || s.arrow_clr == clr)
             {
-                ctrl_bar_pos[0] = i; break;
+                ctrl_bar_pos[0] = i; 
+                break;
             }
         }
         for(int i = bar_width - 1; i >= 0; i--)
@@ -214,17 +231,28 @@ int fishing_(settings& s){
             uint32_t clr = (uint32_t)XGetPixel(check_bar, i, 0) & 0xFFFFFF;
             if(s.ctrl_bar_clr_R == clr || s.arrow_clr == clr)
             {
-                ctrl_bar_pos[1] = i; break;
+                ctrl_bar_pos[1] = i; 
+                break;
             }
         }
+        
+        int raw_priority_zone = bar_width / 2;
         for(int i = 0; i < bar_width - 1; i++)
         {
             uint32_t clr = (uint32_t)XGetPixel(check_bar, i, 0) & 0xFFFFFF;
             if(s.fish_minigame_clr == clr)
             {
-                priority_zone = i; break;
+                raw_priority_zone = i; 
+                break;
             }
         }
+        
+        if(abs(raw_priority_zone - (int)smoothed_target) < 8)
+        {
+            smoothed_target = (target_alpha * raw_priority_zone) + ((1.0f - target_alpha) * smoothed_target);
+        }
+        int priority_zone = (int)smoothed_target;
+        
         XDestroyImage(check_bar);
 
         auto current_time = chrono::high_resolution_clock::now();
@@ -236,6 +264,10 @@ int fishing_(settings& s){
         if(last_bar_pos >= 0.0f && dt > 0.0f)
         {
             float raw_velocity = (current_bar_pos - last_bar_pos) / dt;
+            if(fabsf(raw_velocity - smoothed_velocity) > 0.5f)
+            {
+                raw_velocity = smoothed_velocity;
+            }
             smoothed_velocity = (alpha * raw_velocity) + ((1.0f - alpha) * smoothed_velocity);
         }
         last_bar_pos = current_bar_pos;
@@ -255,50 +287,34 @@ int fishing_(settings& s){
 
         float control_output = (Kp * smooth_predicted_error) - dampening_force;
 
-        if(smoothed_velocity > max_velocity)
+        bool should_hold = is_holding;
+
+        if(abs(raw_distance) < deadband)
         {
-            if(is_holding)
-            {
-                XTestFakeButtonEvent(s.display, 1, 0, CurrentTime);
-                XFlush(s.display);
-                is_holding = false;
-            }
-        } 
-        else if(raw_distance > 0.0f && raw_distance < coast_zone && smoothed_velocity > 0.08f)
-        {
-            if(is_holding)
-            {
-                XTestFakeButtonEvent(s.display, 1, 0, CurrentTime);
-                XFlush(s.display);
-                is_holding = false;
-            }
+            should_hold = is_holding;
         }
-        else if(abs(raw_distance) < deadband && smoothed_velocity > 0.0f)
+        else if(smoothed_velocity > max_velocity && is_holding)
         {
-            if(is_holding)
-            {
-                XTestFakeButtonEvent(s.display, 1, 0, CurrentTime);
-                XFlush(s.display);
-                is_holding = false;
-            }
-        } 
-        else if(control_output > 0.0f)
+            should_hold = false;
+        }
+        else if(raw_distance > 0.0f && raw_distance < coast_zone && smoothed_velocity > 0.15f)
         {
-            if(!is_holding)
-            {
-                XTestFakeButtonEvent(s.display, 1, 1, CurrentTime);
-                XFlush(s.display);
-                is_holding = true;
-            }
-        } 
-        else
+            should_hold = false;
+        }
+        else if(control_output > 0.2f)
         {
-            if(is_holding)
-            {
-                XTestFakeButtonEvent(s.display, 1, 0, CurrentTime);
-                XFlush(s.display);
-                is_holding = false;
-            }
+            should_hold = true;
+        }
+        else if(control_output < -0.2f)
+        {
+            should_hold = false;
+        }
+
+        if(should_hold != is_holding)
+        {
+            XTestFakeButtonEvent(s.display, 1, should_hold ? 1 : 0, CurrentTime);
+            XFlush(s.display);
+            is_holding = should_hold;
         }
 
         usleep(6000);
@@ -308,12 +324,24 @@ int fishing_(settings& s){
 int main()
 {
     settings s;
-    while(1)
+    
+    cout << "\033[32mMacro started. Press Ctrl+C to stop.\033[0m" << endl
+         << "\033[32mAlt + Tab to switch to the right window.\033[0m" << endl;
+    
+    while(!should_exit)
     {
-        if(s.casting) cast_(s);
-        if(s.shaking) shake_(s);
-        if(s.fishing) {sleep(1);fishing_(s);}
-        usleep(600000);
+        if(s.casting) cast_rod(s);
+        if(should_exit) break;
+        usleep(10000);
+        
+        if(s.shaking) shake_rod(s);
+        if(should_exit) break;
+        usleep(10000);
+        
+        if(s.fishing) play_minigame(s);
+        usleep(30000);
     }
+    
+    cout << "\033[32mMacro stopped.\033[0m" << endl;
     return 0;
 }
