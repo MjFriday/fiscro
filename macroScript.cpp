afa112e03e2b0e92e1afcc9813a40c78b0ca2000
MjFriday <7amlhaalan@gmail.com>
2026-09-12 02:14:00 +0300

    added auto reconnect and fixed the mini game thing (again)

--- macroScript.cpp
+++ macroScript.cpp
@@ -23,16 +23,21 @@ const string default_settings =
     "# friend boost icon location. only 1 green pixel from it is needed\n"
     "friend_boost:30,718\n"
     "# you must have the rod in your hot bar and have a pixel location of the white outline around it\n"
-    "rod_equip:373,700\n\n"
+    "rod_equip:373,700\n"
+    "reconnect_button:700,460\n"
+    "teleport_button:750,430\n"
+    "roblox_logo:32,32\n"
     "# pixel color settings (very self explanatory)\n"
     "ctrl_bar_clr_L:0xF1F1F1\n"
     "ctrl_bar_clr_R:0xF1F1F1\n"
     "arrow_clr:0x868686\n"
     "fish_minigame_clr:0x434B5B\n"
-    "friend_boost_clr:0x9BFF9B\n\n"
+    "friend_boost_clr:0x9BFF9B\n"
+    "roblox_logo_clr:0xF7F7F8\n"
     "casting:1\n"
     "shaking:0\n"
-    "fishing:1\n";
+    "fishing:1\n"
+    "auto_reconnect:1\n";
 
 struct settings;
 void load_settings(settings& s);
@@ -43,11 +48,11 @@ struct settings
     Window root = 0;
     int screen_res[2],
         bar_start[2], bar_end[2],
-        friend_boost[2], rod_equip[2];
+        friend_boost[2], rod_equip[2],
+        reconnect_button[2], teleport_button[2], roblox_logo[2];
     uint32_t ctrl_bar_clr_L, ctrl_bar_clr_R, 
-        arrow_clr,
-        fish_minigame_clr, friend_boost_clr;
-    bool casting, shaking, fishing;
+        arrow_clr, fish_minigame_clr, friend_boost_clr, roblox_logo_clr;
+    bool casting, shaking, fishing, auto_reconnect;
 
     settings()
     {
@@ -95,14 +100,19 @@ void load_settings(settings& s)
             else if(key == "bar_end") str_to_x_and_y(value, s.bar_end);
             else if(key == "friend_boost") str_to_x_and_y(value, s.friend_boost);
             else if(key == "rod_equip") str_to_x_and_y(value, s.rod_equip);
+            else if(key == "reconnect_button") str_to_x_and_y(value, s.reconnect_button);
+            else if(key == "teleport_button") str_to_x_and_y(value, s.teleport_button);
+            else if(key == "roblox_logo") str_to_x_and_y(value, s.roblox_logo);
             else if(key == "ctrl_bar_clr_L") s.ctrl_bar_clr_L = stoul(value, nullptr, 16);
             else if(key == "ctrl_bar_clr_R") s.ctrl_bar_clr_R = stoul(value, nullptr, 16);
             else if(key == "arrow_clr") s.arrow_clr = stoul(value, nullptr, 16);
             else if(key == "fish_minigame_clr") s.fish_minigame_clr = stoul(value, nullptr, 16);
             else if(key == "friend_boost_clr") s.friend_boost_clr = stoul(value, nullptr, 16);
+            else if(key == "roblox_logo_clr") s.roblox_logo_clr = stoul(value, nullptr, 16);
             else if(key == "casting") s.casting = stoi(value);
             else if(key == "shaking") s.shaking = stoi(value);
             else if(key == "fishing") s.fishing = stoi(value);
+            else if(key == "auto_reconnect") s.auto_reconnect = stoi(value);
         }
         fetchfile.close();
     }
@@ -118,19 +128,34 @@ void load_settings(settings& s)
         s.bar_end[0]=958; s.bar_end[1]=664;
         s.friend_boost[0]=30; s.friend_boost[1]=718;
         s.rod_equip[0]=373; s.rod_equip[1]=700;
+        s.reconnect_button[0]=700; s.reconnect_button[1]=460;
+        s.teleport_button[0]=750; s.teleport_button[1]=430;
+        s.roblox_logo[0]=32; s.roblox_logo[1]=32; 
         s.ctrl_bar_clr_L=0xF1F1F1; s.ctrl_bar_clr_R=0xF1F1F1;
-        s.arrow_clr=0x868686;
+        s.arrow_clr=0x868686; s.roblox_logo_clr=0xF7F7F8;
         s.fish_minigame_clr=0x434B5B; s.friend_boost_clr=0x9BFF9B;
-        s.casting=1; s.shaking=1; s.fishing=1;
+        s.casting=1; s.shaking=1; s.fishing=1; s.auto_reconnect=1;
     }
 }
-
+bool same_pixel(settings& s, int x, int y, uint32_t color)
+{
+    XImage* check = XGetImage(s.display, s.root, x, y, 1, 1, AllPlanes, ZPixmap);
+    if (!check) return false;
+    uint32_t pixel_color = (uint32_t)XGetPixel(check, 0, 0) & 0xFFFFFF;
+    XDestroyImage(check);
+    return (color == pixel_color);
+}
 bool frnd_icon_check(settings& s)
 {
     XImage* check = XGetImage(s.display, s.root, s.friend_boost[0], s.friend_boost[1], 1, 1, AllPlanes, ZPixmap);
     bool active = (s.friend_boost_clr == ((uint32_t)XGetPixel(check, 0, 0) & 0xFFFFFF));
     XDestroyImage(check); 
     return active;
+}
+
+bool roblox_icon_check(settings& s)
+{
+    return same_pixel(s, s.roblox_logo[0], s.roblox_logo[1], s.roblox_logo_clr);
 }
 
 void rod_equiping(settings& s)
@@ -141,7 +166,7 @@ void rod_equiping(settings& s)
         XTestFakeKeyEvent(s.display, XKeysymToKeycode(s.display, XK_1), 1, CurrentTime);
         XFlush(s.display); usleep(10000);
         XTestFakeKeyEvent(s.display, XKeysymToKeycode(s.display, XK_1), 0, CurrentTime);
-        XFlush(s.display); usleep(10000);
+        XFlush(s.display); usleep(200000);
     }
     XDestroyImage(check);
 }
@@ -150,6 +175,10 @@ void cast_rod(settings& s)
 {
     if(frnd_icon_check(s))
     {
+        XTestFakeKeyEvent(s.display, XKeysymToKeycode(s.display, XK_1), 1, CurrentTime);
+        XFlush(s.display); usleep(10000);
+        XTestFakeKeyEvent(s.display, XKeysymToKeycode(s.display, XK_1), 0, CurrentTime);
+        XFlush(s.display); usleep(300000);
         rod_equiping(s);
         XTestFakeMotionEvent(s.display, -1, (s.screen_res[0] / 2), ((s.screen_res[1] / 2)+5), CurrentTime);
         XFlush(s.display); usleep(1000);
@@ -180,6 +209,48 @@ void shake_rod(settings& s)
     }
 }
 
+int auto_reconnect(settings& s)
+{
+    bool disconnected = same_pixel(s, s.reconnect_button[0], s.reconnect_button[1], 0xFFFFFF);
+    bool roblox_icon = same_pixel(s, s.roblox_logo[0], s.roblox_logo[1], s.roblox_logo_clr);
+    if (disconnected && !roblox_icon)
+    {
+        XTestFakeMotionEvent(s.display, -1, s.reconnect_button[0], s.reconnect_button[1], CurrentTime); XFlush(s.display); usleep(100);
+        XTestFakeMotionEvent(s.display, -1, s.reconnect_button[0]+1, s.reconnect_button[1], CurrentTime); XFlush(s.display); usleep(100);
+        XTestFakeButtonEvent(s.display, 1, 1, CurrentTime); XFlush(s.display); usleep(100000);
+        XTestFakeButtonEvent(s.display, 1, 0, CurrentTime); XFlush(s.display); usleep(100000);
+        XTestFakeButtonEvent(s.display, 1, 1, CurrentTime); XFlush(s.display); usleep(100000);
+        XTestFakeButtonEvent(s.display, 1, 0, CurrentTime); XFlush(s.display);
+        sleep(50);
+        XTestFakeButtonEvent(s.display, 1, 1, CurrentTime); XFlush(s.display); usleep(100000);
+        XTestFakeButtonEvent(s.display, 1, 0, CurrentTime); XFlush(s.display);
+        sleep(15);
+        for(int i=0; i<3; i++)
+        {
+            XTestFakeKeyEvent(s.display, XKeysymToKeycode(s.display, XK_8), True, CurrentTime);
+            XTestFakeButtonEvent(s.display, 1, 1, CurrentTime); XFlush(s.display); usleep(30000);
+            XTestFakeButtonEvent(s.display, 1, 0, CurrentTime); XFlush(s.display);
+            XTestFakeMotionEvent(s.display, -1, s.teleport_button[0], s.teleport_button[1], CurrentTime); XFlush(s.display); usleep(100);
+            XTestFakeButtonEvent(s.display, 1, 1, CurrentTime); XFlush(s.display); usleep(30000);
+            XTestFakeButtonEvent(s.display, 1, 0, CurrentTime); XFlush(s.display);
+        }
+        sleep(5);
+        for(int i=0; i<7; i++)
+        {
+            XTestFakeButtonEvent(s.display, 5, 1, CurrentTime); XFlush(s.display); usleep(200000);
+            XTestFakeButtonEvent(s.display, 5, 0, CurrentTime); XFlush(s.display);
+        }
+        XTestFakeKeyEvent(s.display, XKeysymToKeycode(s.display, XK_Tab), True, CurrentTime);
+        XFlush(s.display);
+        usleep(20000);
+        XTestFakeKeyEvent(s.display, XKeysymToKeycode(s.display, XK_Tab), False, CurrentTime);
+        XFlush(s.display);
+        usleep(20000);
+    }
+
+    return 0;
+}
+
 void play_minigame(settings& s)
 {
     int mini_game_bar_[3] = {s.bar_start[0], s.bar_end[0], (s.bar_start[1] + s.bar_end[1]) / 2};
@@ -204,9 +275,20 @@ void play_minigame(settings& s)
 
     auto last_time = chrono::high_resolution_clock::now();
     int frnd_check_count = 0;
-    
+    auto start_time = chrono::steady_clock::now();
     while(!should_exit)
     {
+        auto current_steady_time = chrono::steady_clock::now();
+        if(chrono::duration_cast<chrono::seconds>(current_steady_time - start_time).count() >= 40)
+        {
+            if(is_holding)
+            {
+                XTestFakeButtonEvent(s.display, 1, 0, CurrentTime);
+                XFlush(s.display);
+            }
+
+            return;
+        }
         if(frnd_icon_check(s)) 
         {
             frnd_check_count++;
@@ -320,28 +402,43 @@ void play_minigame(settings& s)
         usleep(6000);
     }
 }
+void alt_tab(settings& s) {
+    KeyCode alt = XKeysymToKeycode(s.display, XK_Alt_L);
+    KeyCode tab = XKeysymToKeycode(s.display, XK_Tab);
 
+    XTestFakeKeyEvent(s.display, alt, True, CurrentTime);
+    XFlush(s.display);
+    usleep(20000); 
+
+    XTestFakeKeyEvent(s.display, tab, True, CurrentTime);
+    XFlush(s.display);
+    usleep(20000);
+    XTestFakeKeyEvent(s.display, tab, False, CurrentTime);
+    XFlush(s.display);
+    usleep(20000);
+
+    XTestFakeKeyEvent(s.display, alt, False, CurrentTime);
+    XFlush(s.display);
+}
 int main()
 {
     settings s;
     
     cout << "\033[32mMacro started. Press Ctrl+C to stop.\033[0m" << endl
          << "\033[32mAlt + Tab to switch to the right window.\033[0m" << endl;
-    
+    alt_tab(s);
     while(!should_exit)
     {
         if(s.casting) cast_rod(s);
-        if(should_exit) break;
         usleep(10000);
         
         if(s.shaking) shake_rod(s);
-        if(should_exit) break;
         usleep(10000);
         
         if(s.fishing) play_minigame(s);
         usleep(30000);
+
+        if(s.auto_reconnect) auto_reconnect(s);
     }
-    
-    cout << "\033[32mMacro stopped.\033[0m" << endl;
     return 0;
 }
